1e0cfd9d2a5c227236afaa1306e06d10ac2178c9
JacksonBiriukov <git.user.birukov0709@gmail.com>
2024-02-28 21:42:30 +0300

    Вывод в консоль математических операций над 2 числами

--- src/main.java
+++ src/main.java
@@ -2,9 +2,13 @@ import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>:");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " +
-                text.length());
+        System.out.println("Введите первое число:");
+       int number1 = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число:");
+       int number2 = new Scanner(System.in).nextInt();
+        System.out.println("Сумма двух чисел: " + (number1 + number2));
+        System.out.println("Разность двух чисел: " + (number1 - number2));
+        System.out.println("Произведение двух чисел: " + (number1 * number2));
+        System.out.println("Частное двух чисел: " + ((double)number1 / number2));
     }
 }
