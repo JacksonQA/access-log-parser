64a177118f4837c29b9067c0c2cb8b8a653bff52
JacksonBiriukov <git.user.birukov0709@gmail.com>
2024-03-04 22:52:23 +0300

    метод проврки регистра символа

--- src/main.java
+++ src/main.java
@@ -3,10 +3,11 @@ import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) {
-        System.out.println("Результат метода fraction: " + fraction(5.3));
-        System.out.println("Результат метода sumLastNums: " + sumLastNums(4568923));
-        System.out.println("Результат метода charToNum: " + charToNum('5'));
-        System.out.println("Результат метода is2Digits: " + is2Digits(10));
+//        System.out.println("Результат метода fraction: " + fraction(5.3));
+//        System.out.println("Результат метода sumLastNums: " + sumLastNums(4568923));
+//        System.out.println("Результат метода charToNum: " + charToNum('5'));
+//        System.out.println("Результат метода is2Digits: " + is2Digits(10));
+        System.out.println("Результат метода isUpperCase: " + isUpperCase('a'));
     }
     public static double fraction(double x) {
         double res = Math.round(x%1 * 1000.0) / 1000.0 ;
@@ -23,6 +24,10 @@ public class main {
     }
     public static boolean is2Digits(int x){
         boolean res = x > 9;
+        return res;
+    }
+    public static boolean isUpperCase(char x){
+        boolean res = x < 91;
         return res;
     }
 }
