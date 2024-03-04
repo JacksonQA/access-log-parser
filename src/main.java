e7c663c8b6169baa470fcf8ca4db76bea20bedce
JacksonBiriukov <git.user.birukov0709@gmail.com>
2024-03-04 22:07:30 +0300

    метод, возвращающий результат сложения двух последних знаков числа

--- src/main.java
+++ src/main.java
@@ -3,10 +3,16 @@ import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) {
-        System.out.println("Результат: " + fraction(5.3));
+        System.out.println("Результат 1 метода: " + fraction(5.3));
+        System.out.println("Результат 1 метода: " + sumLastNums(4568923));
     }
     public static double fraction(double x) {
         double res = Math.round(x%1 * 1000.0) / 1000.0 ;
         return res;
+    }
+    public static int sumLastNums(int x) {
+        int first= x%10;
+        int second = (x/10)%10;
+        return first + second;
     }
 }
