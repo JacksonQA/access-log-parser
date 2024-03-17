00d7d1c81a547a9bbf62b185fdb9c1f0a66b7a8e
JacksonBiriukov <git.user.birukov0709@gmail.com>
2024-03-17 14:53:58 +0300

    метод возвращающий все числа до заданного

---
+++ src/PracticeLoops.java
@@ -0,0 +1,11 @@
+public class PracticeLoops {
+
+    public static String listNums(int x){
+        String q = "";
+        for (int i = 0; i <= x; i++) {
+            q += i +" ";
+        }
+        return q;
+    }
+
+}
--- src/main.java
+++ src/main.java
@@ -1,6 +1,6 @@
 public class main {
     public static void main(String[] args) {
-    Conditions.printDays("суббота");
+        System.out.println(PracticeLoops.listNums(5));
     }
     public static double fraction(double x) {
         double res = Math.round(x%1 * 1000.0) / 1000.0 ;
@@ -12,7 +12,7 @@ public class main {
         return first + second;
     }
     public static int charToNum(char x){
-      int res = x - 48;
+        int res = x - 48;
         return res;
     }
     public static boolean is2Digits(int x){
@@ -24,8 +24,8 @@ public class main {
         return res;
     }
     public static boolean isInRange(int a, int b, int num){
-      boolean res1 = num <= a && num >= b;
-      boolean res2 = num >= a && num <= b;
+        boolean res1 = num <= a && num >= b;
+        boolean res2 = num >= a && num <= b;
         return res1 | res2;
     }
     public static boolean isDivisor (int a, int b) {
@@ -33,7 +33,7 @@ public class main {
         return res;
     }
     public static boolean isEqual (int a, int b, int c){
-         boolean res = a == b && a == c;
+        boolean res = a == b && a == c;
         return res;
     }
     public static int lastNumSum(int a, int b){
