73ea6ca4eb46169249b042c06a8ce51f465e889c
JacksonBiriukov <git.user.birukov0709@gmail.com>
2024-03-20 00:31:12 +0300

    Проверка валидного указания пути к файлу

--- src/main.java
+++ src/main.java
@@ -1,10 +1,27 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>:");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " +
-                text.length());
+        int N = 0;
+        while (true) {
+            String path = new Scanner(System.in).nextLine();
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+            if (fileExists && !isDirectory) {
+                System.out.println("Путь указан верно");
+                N += 1;
+                System.out.println("Это файл номер " + N);
+            } else if (!fileExists) {
+                System.out.println("указанный файл не существует");
+                continue;
+            } else if (isDirectory) {
+                System.out.println("указанный путь является путем к папке");
+                continue;
+            }
+        }
     }
 }
+
+
